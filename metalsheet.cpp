1555c471710a234f6b9e28b0c6f1b3a3dcef5b7e
Jan <fiszer@student.agh.edu.pl>
2021-04-29 13:10:37 +0200

    minimum working propeply

--- metalsheet.cpp
+++ metalsheet.cpp
@@ -36,6 +36,14 @@ void MetalSheet::read(istream & is){
     width = atoi(s.c_str());
 }
 
+int whole_area(const vector<MetalSheet>& metalsheets){
+    int area = 0;
+    for (auto metalsheet : metalsheets)
+    {
+        area += metalsheet.area() * metalsheet.quantity;
+    }
+    return area;
+}
 
 class OptimizedMetalSheet:public MetalSheet{
     int x;
@@ -51,14 +59,14 @@ bool OptimizedMetalSheet::does_cross(OptimizedMetalSheet & mt)
 
 class ToOptimize
 {
+public:
     vector<MetalSheet> small;
     vector<MetalSheet> big;
-public:
     void read_small(istream & is);
     void write_small(ostream & os);
     void read_big(istream & is);
     void write_big(ostream & os);
-    int whole_area()const;
+    //static int whole_area(vector<MetalSheet> & metalsheets);
     vector<MetalSheet> needed_big_sheets()const;
 };
 
@@ -104,30 +112,37 @@ void ToOptimize::write_big(ostream &os)
         metalSheet.write(os);
     }
 }
-int ToOptimize::whole_area() const {
-    int area = 0;
-    for (auto metalsheet : small)
-    {
-        area += metalsheet.area() * metalsheet.quantity;
-    }
-    return area;
-}
-vector<MetalSheet> ToOptimize::needed_big_sheets() const {
-    vector<MetalSheet> needed_big_sheets;
-    int whole_area = this -> whole_area();
-    //int *areas_of_big_sheets = static_cast<int *>(malloc(sizeof(int) * big.size()));
-    int areas_of_big_sheets[10];
-    for (int i=0; i < big.size(); ++i)
-    {
-        areas_of_big_sheets[i] = big[i].area();
-    }
 
+vector<MetalSheet> ToOptimize::needed_big_sheets() const {
+    vector<MetalSheet> needed_big_sheets, left_small_sheets = small;
+
+    //int *areas_of_big_sheets = static_cast<int *>(malloc(sizeof(int) * big.size()));
+    int area_of_big_sheet;
+    int area_of_left_smalls;
     for (int i=0; i < big.size(); ++i)
     {
         int quantity_big = 0;
+        area_of_big_sheet = big[i].area();
+        area_of_left_smalls = whole_area(left_small_sheets);
         bool out_of_stock = true;
-        while (whole_area > 0 && out_of_stock)
+        while (area_of_left_smalls > 0 && out_of_stock)
         {
+            for (MetalSheet small_sheet : small)
+            {
+                if (small_sheet.high > big[i].high || small_sheet.width > big[i].width)
+                {
+                    area_of_left_smalls -= small_sheet.area();
+                    left_small_sheets.clear();
+                    left_small_sheets.push_back(small_sheet);
+                }
+                small_sheet.rotate();
+                if (small_sheet.high > big[i].high || small_sheet.width > big[i].width)
+                {
+                    area_of_left_smalls -= small_sheet.area();
+                    left_small_sheets.clear();
+                    left_small_sheets.push_back(small_sheet);
+                }
+            }
             if (quantity_big >= big[i].quantity)
             {
                 cerr << "ERROR: Out of stock, sheet number " << i << endl;
@@ -135,7 +150,7 @@ vector<MetalSheet> ToOptimize::needed_big_sheets() const {
                 break;
             }
             quantity_big++;
-            whole_area -= areas_of_big_sheets[i];
+            area_of_left_smalls -= area_of_big_sheet;
         }
         MetalSheet to_add;
         to_add.quantity = quantity_big;
@@ -145,6 +160,8 @@ vector<MetalSheet> ToOptimize::needed_big_sheets() const {
     }
     return needed_big_sheets;
 }
+
+
 
 //TESTY
 
@@ -170,7 +187,7 @@ void whole_area_test()
         return;
     }
     test_tp.read_small(file);
-    cout << endl <<test_tp.whole_area()<<endl;
+    cout << endl << whole_area(test_tp.small) <<endl;
 }
 void needed_big_sheets_test()
 {
@@ -185,7 +202,7 @@ void needed_big_sheets_test()
     test_tp.read_small(file_small);
     test_tp.read_big(file_big);
     test_tp.write_small(cout);
-    test_tp.write_big(cout);
+    //test_tp.write_big(cout);
     vector<MetalSheet> to_print = test_tp.needed_big_sheets();
     for (auto & metal : to_print)
     {
